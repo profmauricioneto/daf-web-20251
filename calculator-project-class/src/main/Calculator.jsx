e594cb4cbc8dcfa2e8e7f58003a8f991dfb44c82
Maurício Moreira Neto <89693991+profmauricioneto@users.noreply.github.com>
2025-03-19 08:50:24 -0300

    add: inicio do projeto calculadora.

---
+++ calculator-project-class/src/main/Calculator.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from 'react';
+import './Calculator.css';
+
+export default class Calculator extends Component {
+    render() {
+        return (
+            <div className='calculator'>
+
+            </div>
+        );
+    }
+}
